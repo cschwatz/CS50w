66ef718bc3a26eecbf6044492e58afec5f5f0ca5
cschwatz <criistianrs@hotmail.com>
2023-08-23 14:13:06 -0300

    index page with working liks to the articles. Notfound page working. Random article working

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -6,9 +6,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:page_name>", views.page, name="page"),
     path("css", views.css, name="css"),
     path("django", views.django, name="django"),
     path("git", views.git, name="git"),
     path("html", views.html, name="html"),
-    path("python", views.python, name="python")
+    path("python", views.python, name="python"),
 ]
--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,36 +1,62 @@
+from random import randint
 from django.shortcuts import render
 
 from . import util
-
-lowercase_names = [entry.lower() for entry in util.list_entries()]
-
 def index(request):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "lower_entries": lowercase_names,
+        "random_article": random_article,
     })
 
 def css(request):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
     return render(request, "encyclopedia/css.html", {
-        "entry": util.get_entry("CSS")
+        "entry": util.get_entry("CSS"),
+        "random_article": random_article,
     })
 
 def django(request):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
     return render(request, "encyclopedia/django.html", {
-        "entry": util.get_entry("Django")
+        "entry": util.get_entry("Django"),
+        "random_article": random_article,
     })
 
 def git(request):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
     return render(request, "encyclopedia/git.html", {
-        "entry": util.get_entry("Git")
+        "entry": util.get_entry("Git"),
+        "random_article": random_article,
     })
 
 def html(request):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
     return render(request, "encyclopedia/html.html", {
-        "entry": util.get_entry("HTML")
+        "entry": util.get_entry("HTML"),
+        "random_article": random_article,
     })
 
 def python(request):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
     return render(request, "encyclopedia/python.html", {
-        "entry": util.get_entry("Python")
+        "entry": util.get_entry("Python"),
+        "random_article": random_article,
     })
+
+def page(request, page_name):
+    random_article = util.list_entries()[randint(0, len(util.list_entries()))]
+    #creates a generator and returns (with the 'next' function) the first occurence in the conditional, else returns a default value (None in this case)
+    entry_name = next((entry for entry in util.list_entries() if entry.lower() == page_name), None)
+    #checks if the name entered at the url directly corresponds to any article that is in 'entries'
+    #if article does not exist, renders the 'notfound' page
+    if not util.get_entry(entry_name):
+        return render(request, "encyclopedia/notfound.html", {
+            "page_name": page_name,
+            "random_article": random_article,
+        })
+    else: #if found an article, render that page's article
+        return render(request, f"encyclopedia/{page_name}.html", {
+            "entry": util.get_entry(entry_name),
+            "random_article": random_article,
+        })
