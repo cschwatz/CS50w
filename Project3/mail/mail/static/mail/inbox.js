e0c1eb8cbcaf0622a0d7a21125691b4da4468c94
cschwatz <criistianrs@hotmail.com>
2023-10-18 16:58:53 -0300

    changed emails to div instead of ul

--- Project3/mail/mail/static/mail/inbox.js
+++ Project3/mail/mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // handle email interaction
+  document.querySelector('#mailDiv').addEventListener('click', () => load_email());
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -15,6 +18,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -40,6 +44,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -49,18 +54,41 @@ function load_mailbox(mailbox) {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    let responseData = data; // its an array of results
-    let unorderedList = document.createElement('ul');
+    let responseData = data; // its an array of results (JSON)
+    let allMails = document.createElement('div'); // div that handles all emails in the specific mailbox
+    allMails.id = 'allMails';
     responseData.forEach(element => {
-      let listItem = document.createElement('li');
-      listItem.innerHTML = [element['id'], 
-                            element['sender'], 
-                            element['subject'], 
-                            element['body'], 
-                            element['timestamp']];
-      unorderedList.appendChild(listItem); 
+      let mailDiv = document.createElement('div'); // div for a single email (box that has border)
+      mailDiv.id = 'mailDiv';
+      // create divs for the content
+      let mailSender = document.createElement('div');
+      let mailSubject = document.createElement('div');
+      let mailTime = document.createElement('div');
+      // give ids
+      mailSender.id = 'mailSender';
+      mailSubject.id = 'mailSubject';
+      mailTime.id = 'mailTime';
+      // populate div with necessary content
+      mailSender.innerHTML = element['sender'];
+      mailSubject.innerHTML = element['subject'];
+      mailTime.innerHTML = element['timestamp'];
+      //append to the email div
+      mailDiv.appendChild(mailSender);
+      mailDiv.appendChild(mailSubject);
+      mailDiv.appendChild(mailTime); 
+      //append email to the div that handles all emails
+      allMails.appendChild(mailDiv);
     });
-    document.querySelector('#emails-view').append(unorderedList);
+    //append all emails to div that shows the emails in the HTML
+    document.querySelector('#emails-view').append(allMails);
   });
   
+  function load_email() {
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'block';
+    
+  }
+    
+
 }
